6bf0d747094882f30a0d4ad51bf94e58625d4fa8
HelenaW86 <helena.wiklund@rearq.io>
2022-09-13 20:58:32 +0200

    Co-authored-by: Elin Amrén <elinamren@users.noreply.github.com>

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-const Recipe = ({recipes}) => {
-    const { recipeSlug } = useParams();
-    const theRecipe = recipes.find((r => r.fields.slug === recipeSlug));
-    const recipe = theRecipe?.fields;
-    console.log('THE RECIPE: ', recipe);
-    console.log(recipe?.image?.fields?.file)
+const Recipe = ({ recipes }) => {
+  const { recipeSlug } = useParams();
+  const theRecipe = recipes.find((r => r.fields.slug === recipeSlug));
+  const recipe = theRecipe?.fields;
+  console.log('THE RECIPE: ', recipe);
+  console.log(recipe?.image?.fields?.file)
 
-    return (
-      <article>
-        <h2>{recipe?.title}</h2>
-        <img src={recipe?.image?.fields?.file?.url} alt={recipe?.title} />
-        <div>
-          <span>Tid: {recipe?.time} </span>
-          <span>Personer: {recipe?.serves} </span>
-        </div>
-        <section>
-          <p>
-            <strong>Ingredienser: </strong>
-            {recipe?.ingredients}
-          </p>
-          <p>
-            <strong>Gör så här: </strong>
-            {recipe?.description}
-          </p>
-        </section>
-      </article>
-    );
+  return (
+    <article className="single-recipe">
+      <h2>{recipe?.title}</h2>
+      <img src={recipe?.image?.fields?.file?.url} alt={recipe?.title} />
+      <div>
+        <span>Tid: {recipe?.time} </span>
+        <span>Personer: {recipe?.serves} </span>
+      </div>
+      <section>
+        <p>
+          <strong>Ingredienser: </strong>
+        </p>
+        <p>
+          {recipe?.ingredients}
+        </p>
+        <p>
+          <strong>Gör så här: </strong>
+          {recipe?.description}
+        </p>
+        <ol>
+          <li>HOHOHHOHO</li>
+          <li>HOHOHHOHO</li>
+          <li>HOHOHHOHO</li>
+        </ol>
+      </section>
+    </article>
+  );
 }
- 
+
 export default Recipe;
--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 
-const RecipeCards = ({recipes}) => {
-    return ( 
-        <>
-        { recipes.map((recipe) => {
-            return (
-              <article key={recipe.sys.id}>
-                <Link to={`recept/${recipe.fields.slug}`}> Go!</Link>
-                {/* ${chargerId} */}
-                {console.log()}
-                <h2>{ recipe.fields.title }</h2>
-                <img src={recipe.fields.image.fields.file.url} alt={recipe.fields.title} />
-                <p>{recipe.fields.summary}</p>
-              </article>
-            )
-          })}
-      </>
-     );
+const RecipeCards = ({ recipes }) => {
+  return (
+    <>
+      {recipes.map((recipe) => {
+        return (
+          <article className="recipe-card" key={recipe.sys.id}>
+            <Link to={`recept/${recipe.fields.slug}`}>
+              <h2 className="recipe-title">{recipe.fields.title}</h2>
+              <img className="recipe-img" src={recipe.fields.image.fields.file.url} alt={recipe.fields.title} />
+              <p>{recipe.fields.summary}</p>
+            </Link>
+          </article>
+        )
+      })}
+    </>
+  );
 }
- 
+
 export default RecipeCards;
