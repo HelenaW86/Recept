93567dad07c4b89574c20bd85be9b831b63bf26f
HelenaW86 <helena.wiklund@rearq.io>
2023-11-03 14:43:44 +0100

    env

--- src/client.js
+++ src/client.js
@@ -1,12 +1,12 @@
 import * as contentful from "contentful";
-
+// require("dotenv").config();
 export const client = contentful.createClient({
-  space: "469cy1q2kb0p",
-  accessToken: "4PN9T-YAwqBSRHvIuCwyWcd4-W67YNEEex_LpFOWYaI",
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
 
   // space: process.env.API_SPACE_ID,
-  // accessToken: process.env.API_ACCESS_TOKEN,
+  // accessToken: process.env.REACT_APP_API_ACCESS_TOKEN,
 
   // space: "469cy1q2kb0p",
   // accessToken: "4PN9T-YAwqBSRHvIuCwyWcd4-W67YNEEex_LpFOWYaI",
