1b9aeaa39eb182a7dbd64f57d3774072300676eb
Helena Wiklund <helena.wiklund@live.se>
2024-05-20 16:07:30 +0200

    ...

--- src/client.js
+++ src/client.js
@@ -4,9 +4,3 @@ export const client = contentful.createClient({
   space: process.env.REACT_APP_SPACE_ID,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
-
-  // space: process.env.API_SPACE_ID,
-  // accessToken: process.env.REACT_APP_API_ACCESS_TOKEN,
-
-  // space: "469cy1q2kb0p",
-  // accessToken: "4PN9T-YAwqBSRHvIuCwyWcd4-W67YNEEex_LpFOWYaI",
